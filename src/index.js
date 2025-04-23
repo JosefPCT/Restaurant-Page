d0c2c96a831bebaa1a216d0732857d8a7c4fde75
Josef Tinagan <tinaganjosefperry@gmail.com>
2025-04-23 09:54:34 +0800

    feat: add a tabSwitch logic function in index.js, to clear and create the home page when home button from nav bar is clicked

--- src/index.js
+++ src/index.js
@@ -8,3 +8,21 @@ console.log(test);
 console.log("Initializing...");
 console.log(home.sayHi());
 console.log(home.create());
+
+const buttons = document.querySelectorAll("button");
+const contentContainer = document.getElementById("content");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", tabSwitch);
+});
+
+function tabSwitch(e){
+    console.log(e.target.id);
+
+    if(e.target.id === "home"){
+        console.log("removing div#content contents...");
+        contentContainer.innerHTML = "";
+        console.log("creating home page");
+        home.create();    
+    }
+}
